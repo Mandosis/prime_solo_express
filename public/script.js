f64ff0a0d24e478760fc05b6f232d7396ef1f4d2
Chris Rabuse <rab233@live.com>
2016-04-05 16:48:08 -0500

    Basic functionality in place and working

--- public/script.js
+++ public/script.js
@@ -1,3 +1,11 @@
 $(function() {
-
+  $('.getBalance').click(function() {
+    getBalance();
+  });
 });
+
+function getBalance() {
+  $.get("http://localhost:3000/balance").done(function(response) {
+    $("body").append("<p>" + response + "</p>");
+  }).fail();
+}
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var bank = require('./modules/bank.js');
 var app = express();
 var port = 3000;
 
+app.get('/balance', function(req, res) {
+  console.log("Request made for balance.");
+  res.send(bank.balance() + bank.money());
+});
+
 // Catch all
 app.use(express.static('public'));
 
